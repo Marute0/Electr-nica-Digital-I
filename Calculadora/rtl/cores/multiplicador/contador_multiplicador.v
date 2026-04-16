175f9fda6e5b0793790e8347a226309500a0d6b1
Marute0 <144182315+Marute0@users.noreply.github.com>
2026-04-15 21:42:04 -0500

    Add files via upload

---
+++ Calculadora/rtl/cores/multiplicador/contador_multiplicador.v
@@ -0,0 +1,21 @@
+module contador_multiplicador #(
+    parameter width = 4
+) (
+
+    input clk,
+    input LD,
+    input ADDI,
+
+    output reg [width:0] out_i
+
+);
+
+    always @(posedge clk) begin
+        if (LD) begin
+            out_i <= 0;
+        end else if (ADDI) begin
+            out_i <= out_i + 1;
+        end
+    end
+    
+endmodule
